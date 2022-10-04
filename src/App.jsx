6fe48701efa0ea3fa1bb64f23ccb8659a1b17cab
Ranorle <100183789+ranorle@users.noreply.github.com>
2022-10-04 19:14:50 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React,{Suspense,lazy} from 'react';
+import {Link,Routes,Route} from "react-router-dom";
+const Home=lazy(()=>import(""));
+function App(){
+    return <h1>App122</h1>;
+}
+export default App;
